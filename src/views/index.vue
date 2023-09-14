8e531a7b9f325daaed746eacc493e7ba88eef2d3
北野 <1017895392@qq.com>
2023-09-14 16:34:59 +0800

    chore: subcode

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="">
-    demo test husky
-
     <router-view v-slot="{ Component }">
       <keep-alive>
         <component :is="Component" />
--- src/views/legado/bookshelf/index.vue
+++ src/views/legado/bookshelf/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class=""> 书架 </div>
+  <div class="">
+    书架
+
+    <ul>
+      <li v-for="(item, index) in books" :key="index">{{ item.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
   import { reqBookshelf } from '@/api'
+  import { BookItem } from '@/types'
+
+  const books = ref<BookItem[]>([])
 
   onBeforeMount(async () => {
-    await reqBookshelf()
+    await initBookshelf()
   })
+
+  const initBookshelf = async () => {
+    books.value = await reqBookshelf()
+  }
 </script>
 
 <style lang="scss" scoped></style>
